b9597638d359d5099da3675a91026acf924b58e9
SAA9E <149317974+SAA9E@users.noreply.github.com>
2024-10-08 11:47:35 +0900

    Add files via upload

---
+++ my_1st.py
@@ -0,0 +1,12 @@
+#터미널 켜서 앱실행 가능 
+# streamlit run my_1st.py  +  엔터
+
+#Local URL: http://localhost:8502
+#Network URL: http://192.168.137.114:8502
+#                               건물 : 호실  같은 느낌
+
+import streamlit as st
+
+# 타이틀 출력
+st.title("첫번째 웹 어플 만들기 😍👍")
+
